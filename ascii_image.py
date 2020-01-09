17f65d2b92124187066298f9d399c30516e58d30
Joe Aylor <joe@josephaylor.com>
2020-01-09 16:20:01 -0500

    gitignore

---
+++ ascii_image.py
@@ -0,0 +1,22 @@
+import os
+
+class AsciiImage:
+    def __init__(self, characters):
+        self.characters = characters
+
+    def display(self, line_width=2):
+        print(self.as_text(line_width))
+
+    def as_text(self, line_width=1):
+        text = ""
+        for row in self.characters:
+            for value in row:
+                text += AsciiImage._display_value(value, line_width)
+            text += os.linesep
+        return text
+
+    def _display_value(value, line_width):
+        text = ""
+        for i in range(line_width):
+            text += value
+        return text
